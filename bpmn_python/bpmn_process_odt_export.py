0131f6d9c95a3930eb56bf852dc6a5ca23a63b5f
Marek Łabuz <mrk.labuz@gmail.com>
2018-01-28 00:25:19 +0100

    nested list rendering + goto numbering

--- bpmn_python/bpmn_process_odt_export.py
+++ bpmn_python/bpmn_process_odt_export.py
@@ -8,9 +8,10 @@ import copy
 import errno
 import os
 import sys
-import re
 
-sys.path.append("/Users/marek/Documents/uni/bpmn-python")
+from odf import easyliststyle
+from odf.opendocument import OpenDocumentText
+from odf.text import P, List, ListItem
 
 import bpmn_python.bpmn_python_consts as consts
 import bpmn_python.bpmn_diagram_exception as bpmn_exception
@@ -18,30 +19,13 @@ import bpmn_python.bpmn_import_utils as utils
 import bpmn_python.bpmn_process_csv_export as bpmn_csv_export
 import bpmn_diagram_rep as diagram
 
-from odf import easyliststyle
-from odf.opendocument import OpenDocumentText
-from odf.text import P, List, ListItem
+sys.path.append("/Users/marek/Documents/uni/bpmn-python")
 
 
 class BpmnDiagramGraphOdtExport(object):
     """
     Class that provides implementation of exporting process to ODT functionality
     """
-    gateways_list = ["exclusiveGateway", "inclusiveGateway", "parallelGateway"]
-    tasks_list = ["task", "subProcess"]
-
-    classification_element = "Element"
-    classification_start_event = "Start Event"
-    classification_end_event = "End Event"
-    classification_join = "Join"
-    classification_split = "Split"
-
-    '''
-    Supported start event types: normal, timer, message.
-    Supported end event types: normal, message.
-    '''
-    events_list = ["startEvent", "endEvent"]
-    lanes_list = ["process", "laneSet", "lane"]
 
     def __init__(self):
         pass
@@ -76,19 +60,117 @@ class BpmnDiagramGraphOdtExport(object):
             if exception.errno != errno.EEXIST:
                 raise
 
-        print(export_elements)
-        item_list = []
-
-        for export_element in export_elements:
-            l = len(re.sub(r"([a-z])+", "", export_element["Order"])) - 1
-            prefix = ''.join([">"] * l)
-            item_list.append(prefix + export_element["Activity"])
+        item_list = BpmnDiagramGraphOdtExport.p(export_elements)
+        item_list = BpmnDiagramGraphOdtExport.reassign_goto(item_list, export_elements)
+        item_list = map(lambda item: item["Prefix"] + item["Label"], item_list)
 
         odt_file = directory + filename
         BpmnDiagramGraphOdtExport.write_export_node_to_file(odt_file, item_list)
 
     @staticmethod
+    def reassign_goto(item_list, export_elements):
+        """
+        Reassigning goto activities adequately to the list numbering.
+        :param item_list: TODO,
+        :param export_elements: TODO.
+        """
+        result = item_list[:]
+        for index, item in enumerate(item_list):
+            if item["Label"].startswith("goto"):
+                order = item["Label"].replace("goto ", "")
+                filtered_arr_order = list(filter(lambda elem: elem["Order"] == order, export_elements))
+                if len(filtered_arr_order) > 0:
+                    activity = filtered_arr_order[0]["Activity"]
+                    i = len(item_list) - 1
+                    while i > 0:
+                        if item_list[i]["Label"] == activity:
+                            break
+                        i -= 1
+
+                    prefix = item_list[i]["Prefix"]
+                    depth = len(prefix)
+
+                    position = [1] * (depth + 1)
+                    print(position, depth)
+
+                    i -= 1
+                    while i >= 0 and depth >= 0:
+                        if len(item_list[i]["Prefix"]) == depth:
+                            position[depth] += 1
+
+                        if len(item_list[i]["Prefix"]) < depth:
+                            depth -= 1
+
+                        i -= 1
+
+                    new_prefix = ".".join(map(lambda n: str(n), position)) + "."
+                    result[index]["Label"] = "goto " + new_prefix
+        return result
+
+    @staticmethod
+    def prefix(n):
+        """
+        Reassigning goto activities adequately to the list numbering.
+        :param n: TODO.
+        """
+        return "".join([">"] * n)
+
+    @staticmethod
+    def p(arr, last_order="0", depth=0, last_condition=""):
+        """
+        TODO.
+        :param arr: TODO,
+        :param last_order: TODO,
+        :param depth: TODO,
+        :param last_condition: TODO.
+        """
+        if len(arr) == 0:
+            return []
+
+        first, rest = arr[0], arr[1:]
+
+        order_length = len(last_order)
+        new_depth = depth
+        result = []
+
+        order = first["Order"]
+        activity = first["Activity"]
+        condition = first["Condition"]
+        terminated = first["Terminated"]
+
+        if terminated == "yes":
+            activity = "Terminate"
+
+        if activity == "":
+            return BpmnDiagramGraphOdtExport.p(rest, last_order, depth, last_condition)
+
+        if len(order) < order_length:
+            new_depth -= 2 if last_condition != "" else 1
+
+        if len(order) == order_length and len(order) >= 3 and last_order[:-1] != order[:-1] and condition != "":
+            result.append({"Prefix": BpmnDiagramGraphOdtExport.prefix(new_depth - 1), "Label": "If " + condition})
+
+        new_condition = last_condition
+        if len(order) > order_length:
+            new_condition = condition
+            if condition != "":
+                result.append({"Prefix": BpmnDiagramGraphOdtExport.prefix(new_depth + 1), "Label": "If " + condition})
+                new_depth += 2
+            else:
+                new_depth += 1
+
+        result.append({"Prefix": BpmnDiagramGraphOdtExport.prefix(new_depth), "Label": activity})
+
+        return result + BpmnDiagramGraphOdtExport.p(rest, order, new_depth, new_condition)
+
+    @staticmethod
     def create_list(item_list, indent_delim, style_name):
+        """
+        TODO.
+        :param item_list: TODO,
+        :param indent_delim: TODO,
+        :param style_name: TODO.
+        """
         list_array = []
         last_level = 0
 
@@ -123,17 +205,17 @@ class BpmnDiagramGraphOdtExport(object):
     @staticmethod
     def write_export_node_to_file(odt_file, item_list):
         """
-        Exporting process to ODT file
-        :param odt_file: TODO,
-        :param item_list: TODO
+        Exporting process to ODT file.
+        :param odt_file: ODT file pathname,
+        :param item_list: array of strings with document list items.
         """
-        mixed_list_spec = u'1.!\u273f!a)'
+        mixed_list_spec = '1.,1.,1.,1.,1.,1.'
 
         textdoc = OpenDocumentText()
 
         s = textdoc.styles
 
-        list_style = easyliststyle.styleFromString("mix", mixed_list_spec, "!", "0.8cm", easyliststyle.SHOW_ONE_LEVEL)
+        list_style = easyliststyle.styleFromString("mix", mixed_list_spec, ",", "0.8cm", easyliststyle.SHOW_ONE_LEVEL)
         s.addElement(list_style)
 
         list_element = BpmnDiagramGraphOdtExport.create_list(item_list, ">", "mix")
@@ -144,5 +226,5 @@ class BpmnDiagramGraphOdtExport(object):
 
 if __name__ == "__main__":
     bpmn_graph = diagram.BpmnDiagramGraph()
-    bpmn_graph.load_diagram_from_xml_file(os.path.abspath("pizza-order.bpmn"))
-    bpmn_graph.export_odt_file('./', "pizza.odt")
+    bpmn_graph.load_diagram_from_xml_file(os.path.abspath("tram-process.bpmn"))
+    bpmn_graph.export_odt_file('./', "tram.odt")
