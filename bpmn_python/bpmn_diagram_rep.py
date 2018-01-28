6725d3ef580676effc7fe111072ca509e7242d22
Marek Łabuz <mrk.labuz@gmail.com>
2018-01-28 23:47:08 +0100

    tests

--- bpmn_python/bpmn_diagram_rep.py
+++ bpmn_python/bpmn_diagram_rep.py
@@ -9,8 +9,8 @@ import networkx as nx
 import bpmn_python.bpmn_diagram_exception as bpmn_exception
 import bpmn_python.bpmn_diagram_export as bpmn_export
 import bpmn_python.bpmn_diagram_import as bpmn_import
-import bpmn_process_csv_export as bpmn_csv_export
-import bpmn_process_odt_export as bpmn_odt_export
+import bpmn_python.bpmn_process_csv_export as bpmn_csv_export
+import bpmn_python.bpmn_process_odt_export as bpmn_odt_export
 import bpmn_python.bpmn_process_csv_import as bpmn_csv_import
 import bpmn_python.bpmn_python_consts as consts
 
